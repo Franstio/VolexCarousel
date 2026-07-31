fd71624dafddfeff4b0bb71ffc006f8d2acd2e99
franstio <franstiodiego01@gmail.com>
2026-07-31 15:45:13 +0700

    Inject TCPPLCService into DesktopAppService, update ExecuteAsync
    Added TCPPLCService as a dependency to DesktopAppService via constructor injection. Made ExecuteAsync asynchronous and invoked plcService.WriteCommand("MR701","1") before starting the Avalonia app. Removed Task.CompletedTask return and added necessary using directive.

--- VolexCarousel/Services/DesktopAppService.cs
+++ VolexCarousel/Services/DesktopAppService.cs
@@ -6,22 +6,24 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using VolexCarousel.Core.Services;
 
 namespace VolexCarousel.Services
 {
     public class DesktopAppService : BackgroundService
     {
         private readonly IHostApplicationLifetime hostApplicationLifetime;
-        
-        public DesktopAppService(IHostApplicationLifetime hostApplicationLifetime)
+        private readonly TCPPLCService plcService;
+        public DesktopAppService(IHostApplicationLifetime hostApplicationLifetime,TCPPLCService plcService)
         {
             this.hostApplicationLifetime = hostApplicationLifetime;
+            this.plcService = plcService;
         }
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            await plcService.WriteCommand("MR701","1");
             Program.BuildAvaloniaApp().StartWithClassicDesktopLifetime(Environment.GetCommandLineArgs());
             this.hostApplicationLifetime.StopApplication();
-            return Task.CompletedTask;
         }
     }
 }
