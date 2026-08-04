e95d026d3e0417bf845ef6e38e7efccd6a2f50e7
franstio <franstiodiego01@gmail.com>
2026-08-04 15:48:43 +0700

    Increase delay in SensorBackgroundService to 100ms
    The delay in the finally block of SensorBackgroundService.cs was increased from 10ms to 100ms, resulting in a longer wait between service iterations.

--- VolexCarousel/Services/SensorBackgroundService.cs
+++ VolexCarousel/Services/SensorBackgroundService.cs
@@ -67,7 +67,7 @@ namespace VolexCarousel.Services
                 }
                 finally
                 {
-                    await Task.Delay(TimeSpan.FromMilliseconds(10));
+                    await Task.Delay(TimeSpan.FromMilliseconds(100));
                 }
             }
         }
